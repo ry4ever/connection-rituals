ca58aecdede51832d497738c129b87f6b1fc9adf
ry4ever <88810067+ry4ever@users.noreply.github.com>
2025-09-17 00:36:15 +0800

    Update Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -173,7 +173,7 @@ const Newsletter = () => {
                 ) : (
                   <>
                     <ShoppingCart className="w-5 h-5 mr-2" />
-                    Buy Now - $29
+                    Buy Now - $7.99
                     <ArrowRight className="w-4 h-4 ml-2 transition-transform group-hover:translate-x-1" />
                   </>
                 )}
